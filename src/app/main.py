cb3001efe4a53303d0d2212a2a7ff0d6bd65eaf7
Austin Tercha <aut7@pitt.edu>
2020-11-01 18:57:15 -0500

    finished skeleton gui

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ Script that runs gui.
 """
 import tkinter as tk
 
+
 class MainGUI(object):
 
     def __init__(self):
@@ -11,7 +12,7 @@ class MainGUI(object):
     def create_window(self):
         self.root = tk.Tk()
         self.root.title("Application Selector")
-        self.root.geometry("800x500")
+        self.root.geometry("800x550")
 
     def add_labels(self):
         instructions = tk.Text(self.root, height=5, width=52)
@@ -24,6 +25,59 @@ class MainGUI(object):
         ntpd = tk.Button(self.root, text="Notepad++",
             bd='5', command=None)
         ntpd.pack(side="top")
+
+        btn = tk.Button(self.root, text="Rstudio",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Spyder",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="IBM SAS",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Git",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Jupyter Notebook",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Orange",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Visual Studio Code IDE",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Apache Hadoop",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Apache Spark",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Tableu",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Sonar Qube and Binaries",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Tensorflow",
+            bd='5', command=None)
+        btn.pack(side="top")
+
+        btn = tk.Button(self.root, text="Markdown",
+            bd='5', command=None)
+        btn.pack(side="top")
+
 
     def display(self):
         self.root.mainloop()

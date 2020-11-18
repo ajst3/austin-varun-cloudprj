6e226c70759ebf01699121aa419efcf4d4b2bf9d
Austin Tercha <aut7@pitt.edu>
2020-11-17 20:08:52 -0500

    clean up runner files

--- src/tensorflow/runner.py
+++ src/tensorflow/runner.py
@@ -6,19 +6,15 @@ from socket import socket, gethostbyname, AF_INET, SOCK_STREAM
 class Runner(object):
 
     def main(self):
-        PORT_NUMBER = 8000
-        SIZE = 1024
-
-        hostName = "app"
         SERVER_IP   = '0.0.0.0'
-        mySocket = socket( AF_INET, SOCK_STREAM )
-        mySocket.bind( (SERVER_IP, 7080) ) # Bind to port 8080
+        mySocket = socket(AF_INET, SOCK_STREAM)
+        mySocket.bind((SERVER_IP, 7080)) # Bind to port 8080
         mySocket.listen(5)  # listen for connections
 
         while True:
             (t1, t2) = mySocket.accept() # accept the connection
             # recieve the data from t1 which is the socket sending to us
-            data = t1.recv(SIZE)
+            data = t1.recv(1024)
             print("test")
             os.system("xterm bash")
 
